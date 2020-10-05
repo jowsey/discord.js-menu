e53e40c1746ab125e4e01c0242b0d75d6481bfa6
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2020-10-05 08:25:45 +0100

    A lil' overhaul (v2.0.0)
     - Overhauled JSDoc comments: This should be especially nice for people using editors such as Visual Studio Code with native doc support. Still working on a way to document events but there's only one for now so it's not too big of an issue.
     - Added an event `pageChange` that fires before changing pages to allow for dynamically edited/custom pages.
     - Made menu pages a class. (Also in preparation for an improved dynamic values system)
     - Added permission checking on menu creation with warning
     - Added the special destination "delete". `stop` now only removes the reactions, `delete` completely removes the message.
     - Changed menu behaviour: Menus now require Menu.start() to send and have the functions stop() and delete() for manual control now.
     - Updated project logo.
     - Updated project README.
     - Fixed any problems with DM channels, hopefully.
     - Probably some other stuff I'm forgetting
    
    I've tested this as thoroughly as I can, but there'll always be things that slip under the radar. Please do report any problems you find as an issue.

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-![logo](https://i.imgur.com/nQSqrrk.png)
-<h4 align="center">Automatically create Discord.js embed menus with reactions and multiple completely customisable pages.</h4>
-
-<p align="center">
+<div align="center">
+    <img src="https://i.imgur.com/GnLwz6d.png" width="580">
+    <h4>Automatically create Discord.js embed menus with reactions and multiple completely customisable pages.</h4>
     <a href="https://nodei.co/npm/discord.js-menu/"><img src="https://nodei.co/npm/discord.js-menu.png"></a><br>
     <img alt="npm" src="https://img.shields.io/npm/dw/discord.js-menu">
     <img alt="npm" src="https://img.shields.io/npm/dt/discord.js-menu">
     <img alt="npm (prod) dependency version" src="https://img.shields.io/npm/dependency-version/discord.js-menu/discord.js">
-</p>
+</div>
 
 <p align="center">
     <img src="https://i.imgur.com/mFA31kh.gif" width="480" height="270"> 
@@ -17,88 +16,107 @@ In your project directory:
 ```bash
 npm install discord.js-menu
 ```
-In your code file:
+In your code:
 ```js
 const { Menu } = require('discord.js-menu');
 ```
 It's as easy as that!
 
-### Examples
-I wanted to make menus as easy as possible to create - it can be quite daunting at first, but I don't think it's too bad!
-Creating and sending a message is as easy as importing the Menu class and instantiating one.
-For this example, let's presume we're trying to show the user a help menu, with a list of commands, or similar.
+### Basic Example
+This is one of the simplest menus you can make. It's two set pages that you can alternate between, nothing fancy.
+Hopefully, it should provide a good first look into how you can create reaction menus with the library.
+
 ```js
-const { Client, MessageEmbed } = require('discord.js');
-const { Menu } = require('discord.js-menu');
-const client = new Client();
+const { Client, MessageEmbed } = require('discord.js')
+const { Menu } = require('discord.js-menu')
+const client = new Client()
 
 client.on('message', message => {
-    if (message.content == "help") {
-        // Creating the menu like this will automatically send it and handle reactions - basically everything.
-        // I recommend you import this big array from a secondary file to clean up your code - your choice though.
-        new Menu(message.channel, message.author.id, [
+    if (message.content === "!help") {
+        // Provide a menu with a channel, an author ID to let control the menu, and an array of menu pages.
+        let helpMenu = new Menu(message.channel, message.author.id, [
+            // Each object in this array is a unique page.
             {
-                name: "main",
+                // A page object consists of a name, used as a destination by reactions...
+                name: 'main',
+                // A MessageEmbed to actually send in chat, and...
                 content: new MessageEmbed({
-                    title: "Help",
-                    description: "Commands list:",
-                    fields: [
-                        {
-                            name: "command1",
-                            value: "this command does stuff"
-                        }
-                    ]
+                    title: 'Help Menu',
+                    description: 'This is some helpful info!'
                 }),
+                // A set of reactions with destination names attached.
+                // Note there's also special destination names (read below)
                 reactions: {
-                    "⏹": "stop",
-                    "▶": "next",
-                    "⚙": "otherPage"
+                    '⏹': 'delete',
+                    '▶': 'extra'
                 }
             },
             {
-                name: "otherPage",
+                name: 'extra',
                 content: new MessageEmbed({
-                    title: "More Help!",
-                    description: "Here are some more commands!",
-                    fields: [
-                        {
-                            name: "You get the idea.",
-                            value: "You can create as many of these pages as you like."
-                            // (Each page can only have 20 reactions, though. Discord's fault.)
-                        }
-                    ]
+                    title: 'Extra menu',
+                    description: 'This is another page. You can have as many of these as you want.'
                 }),
                 reactions: {
-                    "⏹": "stop",
-                    "◀": "previous",
-                    "1️⃣": "first"
+                    '◀': 'first'
                 }
             }
-        ]);
+        ])
+        // Run Menu.start() when you're ready to send the menu in chat.
+        // Once sent, the menu will automatically handle everything else.
+        helpMenu.start()
     }
-});
+})
 
-
-client.login(...);
+client.login("your_token")
 ```
-This is a relatively small menu, at only 2 pages. You can theoretically have as many pages as you want - I've tested it with up to 10, but you can totally go higher.
+This is a relatively small menu, at only 2 pages. You could theoretically create an unlimited amount of them if you needed to.
 
-### Creating the menu, in-depth
-```js
-new Menu(channel, userID, pages)
-```
-Channel and userID are both relatively obvious - a channel to send the message to, and the ID of the user you wish to let control the menu - the pages array is a little more complex, though, as you might've seen.  
-`pages` is an array of objects; each object is it's own page in the menu.  
-Every page has three items: a `name`, which is used as a destination for your reactions, the `content` (a MessageEmbed), and a set of `reactions` for that page.  
-Each reaction is an emoji, and its value is a destination you have set. Every page's name is a destination. There are also 5 other destinations that serve various functions.  
+### Special Destinations
+Every page object has three items: a `name`, which is used as a destination for your reactions, the `content` (a MessageEmbed), and a set of `reactions` for that page.  
+In the `reactions` object, each key is an emoji, and its value a destination you have set. Every page's name is a destination, but there are also 6 other destinations that serve various functions.  
 
 **first** - Goes to the first page in the array.  
 **last** - Goes to the last page in the array.  
 **previous** - Goes to the previous page in the array.  
 **next** - Goes to the next page in the array.  
-**stop** - Removes the reactions and deletes the menu message.  
+**stop** - Removes the reactions and stops updating the menu.  
+**delete** - Stops the menu and deletes the message.
 
-Note that calling one of your pages one of these will still technically work - the page would still be accessible using these destinations, ironically - but you wouldn't be able to directly link to it.  
+Note that whilst calling a page one of these wouldn't break anything (the page would still be accessible using, for example, the `next` and `previous` destinations) you wouldn't be able to directly link to it.
+
+### (New!) Events and dynamic pages
+New to v2.0.0 is the ability to create pages that can be entirely unique and dynamic.  
+v2.0.0 adds the "pageChange" event to the Menu class, which fires just before the message actually edits, meaning you can edit the page with any variables you want before it gets fed into the menu.
+Here's an example of creating a menu page based on a user's profile info.
+```js
+/* -- assume boilerplate code here -- */
+client.on('message', message => {
+    let userMenu = new Menu(message.channel, message.author.id, [
+        {
+            name: 'user',
+            content: new MessageEmbed({
+                title: "Your user info!",
+                color: 0x7289DA
+            })
+        }
+    ])
+    userMenu.start()
+
+    userMenu.on('pageChange', destination => {
+        if (destination.name === "user") {
+            destination.content.description = message.author.username + "'s info:"
+            destination.content.addField('Avatar URL:', message.author.avatarURL())
+            destination.content.addField('Are you a bot?', message.author.bot ? "Yes!" : "No...")
+        }
+    })
+})
+```
+And there you have it, a custom menu page just for you!   
+Now, of course one lone page isn't all that exciting - that's why we have reaction menus! Why not expand the menu into a full dashboard of user data with multiple areas? Remember, you can also dynamically edit the `destination.reactions` and `destination.name` if you'd like to spice things up a little.
+  
+I'm well aware this is still a bit of a janky workaround for some cases, and I'm working on making this more streamlined.
 
 ### Acknowledgements
-This project was heavily inspired by Juby210's [discord.js-reaction-menu](https://github.com/Juby210/discord.js-reaction-menu). It's a great project, and I was originally using it, but in the end, creating my own version seemed more worthwhile. Much respect to Juby210, though!
+This project was heavily inspired by Juby210's [discord.js-reaction-menu](https://github.com/Juby210/discord.js-reaction-menu).  
+It's a great project, and I was originally using it, but in the end, creating my own version seemed more worthwhile. Much respect to Juby210, though!
--- index.js
+++ index.js
@@ -1,75 +1,199 @@
-const { TextChannel } = require('discord.js');
+const { EventEmitter } = require('events')
+const { TextChannel, MessageEmbed } = require('discord.js')
+const requiredPerms = ['SEND_MESSAGES', 'EMBED_LINKS', 'ADD_REACTIONS', 'MANAGE_MESSAGES']
 
 /**
- * Create a menu with customisable reactions for every page.
- * Blacklisted page names are: `first, last, previous, next, stop`.
- * Each name, when set to a reaction value, will do as you expect it to.
- * Stop deletes the menu message.
- * @param channel The text channel you want to send the menu in.
- * @param userID The ID of the user you want to let control the menu.
- * @param pages An array of page objects with a name, content MessageEmbed and a set of reactions with page names which lead to said pages.
+ * A page object that the menu can display.
  */
+class Page {
+  /**
+    * Creates a menu page.
+    * @param {String} name The name of this page, used as a destination for reactions.
+    * @param {MessageEmbed} content The MessageEmbed content of this page.
+    * @param {Object} reactions The reactions that'll be added to this page.
+    * @param {Number} index The index of this page in the Menu
+     */
+  constructor (name, content, reactions, index) {
+    this.name = name
+    this.content = content
+    this.reactions = reactions
+    this.index = index
+  }
+}
 
-module.exports.Menu = class {
-    constructor(channel = new TextChannel(), userID, pages = [{name: "main", content: new MessageEmbed().setTitle("Discord.js-Menu Default Page").setLink("https://npm.im/discord.js-menu"), reactions: {"⏹": "stop"}}]) {
-        this.channel = channel;
-        this.userID = userID;
-        this.pages = pages;
-        this.currentPage = pages[0];
-        this.page = 0;
+/**
+ * A menu with customisable reactions for every page.
+ * Blacklisted page names are: `first, last, previous, next, stop, delete`.
+ * These names perform special functions and should only be used as reaction destinations.
+ */
+module.exports.Menu = class extends EventEmitter {
+  /**
+    * Creates a menu.
+    * @param {TextChannel} channel The text channel you want to send the menu in.
+    * @param {String} userID The ID of the user you want to let control the menu.
+    * @param {Object[]} pages An array of page objects with a name, content MessageEmbed and a set of reactions with page names which lead to said pages.
+    * @param {String} pages.name The page's name, used as a destination for reactions.
+    * @param {MessageEmbed} pages.content The page's embed content.
+    * @param {Object} pages.reactions The reaction options that the page has.
+    *
+    * @remarks
+    * Blacklisted page names are: `first, last, previous, next, stop, delete`.
+    * These names perform special functions and should only be used as reaction destinations.
+    */
+  constructor (channel, userID, pages) {
+    super()
+    this.channel = channel
+    this.userID = userID
 
-        channel.send(this.currentPage.content).then(menu => {
-            this.menu = menu;
-            this.react();
-            this.awaitReactions();
-        });
+    const missingPerms = []
+    // this usually means it's a dm channel that hasn't been created
+    if (!this.channel) {
+      this.channel.client.users.cache.get(this.userID).createDM(true)
     }
-    setPage(page = 0) {
-        this.page = page;
-        this.currentPage = this.pages[this.page];
-        this.menu.edit(this.currentPage.content);
-        
-        this.menu.reactions.removeAll();
-        this.reactionCollector.stop();
-        this.react();
-        this.awaitReactions();
-    }
-    react() {
-        for(const reaction in this.currentPage.reactions) {
-            this.menu.react(reaction);
+    if (this.channel.type !== 'dm') {
+      requiredPerms.forEach(perm => {
+        if (!this.channel.permissionsFor(this.channel.client.user).toArray().includes(perm)) {
+          missingPerms.push(perm)
         }
+      })
+      if (missingPerms.length) console.log(`\x1B[96m[discord.js-menu]\x1B[0m Looks like you're missing ${missingPerms.join(', ')} in #${this.channel.name} (${this.channel.guild.name}). This perm is needed for basic menu operation. You'll probably experience problems sending menus in this channel.`)
+    } else {
     }
-    awaitReactions() {
-        this.reactionCollector = this.menu.createReactionCollector((reaction, user) => user.id == this.userID, {time: 180000});
-        this.reactionCollector.on('collect', reaction => {
-            if (this.currentPage.reactions.hasOwnProperty(reaction.emoji.name)) {
-                let destination = this.currentPage.reactions[reaction.emoji.name];
-                switch (destination) {
-                    case "first":
-                        this.setPage(0);
-                        break;
-                    case "last":
-                        this.setPage(this.pages.length - 1);
-                        break;
-                    case "previous":
-                        if (this.page > 0) {
-                            this.setPage(this.page - 1);
-                        }
-                        break;
-                    case "next":
-                        if (this.page < this.pages.length - 1) {
-                            this.setPage(this.page + 1);
-                        }
-                        break;
-                    case "stop":
-                        this.reactionCollector.stop();
-                        this.menu.delete();
-                        break;
-                    default:
-                        this.setPage(this.pages.findIndex(p => p.name == destination));
-                        break;
-                }
+
+    /**
+     * List of pages available to the Menu.
+     * @type {Page[]}
+     */
+    this.pages = []
+
+    let i = 0
+    pages.forEach(page => {
+      this.pages.push(new Page(page.name, page.content, page.reactions, i))
+      i++
+    })
+
+    /**
+     * The page the Menu is currently displaying in chat.
+     * @type {Page}
+     */
+    this.currentPage = this.pages[0]
+    /**
+     * The index of the Pages array we're currently on.
+     */
+    this.pageIndex = 0
+  }
+
+  /**
+   * Send the Menu and begin listening for reactions.
+   */
+  start () {
+    this.emit('pageChange', this.currentPage)
+    this.channel.send(this.currentPage.content).then(menu => {
+      this.menu = menu
+      this.addReactions()
+      this.awaitReactions()
+    }).catch(error => {
+      if (this.channel.type === 'dm') {
+        console.log(`\x1B[96m[discord.js-menu]\x1B[0m ${error.toString()} (whilst trying to send menu message in DMs) | The person you're trying to DM (${this.channel.client.users.cache.get(this.userID).tag}) probably has DMs turned off.`)
+      } else {
+        console.log(`\x1B[96m[discord.js-menu]\x1B[0m ${error.toString()} (whilst trying to send menu message) | You're probably missing 'SEND_MESSAGES' or 'EMBED_LINKS' in #${this.channel.name} (${this.channel.guild.name}), needed for sending the menu message.`)
+      }
+    })
+  }
+
+  /**
+   * Stop listening for new reactions.
+   */
+  stop () {
+    if (this.reactionCollector) {
+      this.reactionCollector.stop()
+      this.clearReactions()
+    }
+  }
+
+  /**
+   * Delete the menu message.
+   */
+  delete () {
+    if (this.reactionCollector) this.reactionCollector.stop()
+    if (this.menu) this.menu.delete()
+  }
+
+  /**
+   * Remove all reactions from the menu message.
+   */
+  clearReactions () {
+    if (this.menu) {
+      this.menu.reactions.removeAll().catch(error => {
+        console.log(`\x1B[96m[discord.js-menu]\x1B[0m ${error.toString()} (whilst trying to remove message reactions) | You're probably missing 'MANAGE_MESSAGES' in #${this.channel.name} (${this.channel.guild.name}), needed for removing reactions when changing pages.`)
+      })
+    }
+  }
+
+  /**
+   * Jump to a new page in the Menu.
+   * @param {Number} page The index of the page the Menu should jump to.
+   */
+  setPage (page = 0) {
+    this.emit('pageChange', this.pages[page])
+
+    this.pageIndex = page
+    this.currentPage = this.pages[this.pageIndex]
+    this.menu.edit(this.currentPage.content)
+
+    this.clearReactions()
+    this.reactionCollector.stop()
+    this.addReactions()
+    this.awaitReactions()
+  }
+
+  /**
+   * React to the new page with all of it's defined reactions
+   */
+  addReactions () {
+    for (const reaction in this.currentPage.reactions) {
+      this.menu.react(reaction).catch(error => {
+        console.log(`\x1B[96m[discord.js-menu]\x1B[0m ${error.toString()} (whilst trying to add reactions to message) | You're probably missing 'ADD_REACTIONS' in #${this.channel.name} (${this.channel.guild.name}), needed for adding reactions to the page.`)
+      })
+    }
+  }
+
+  /**
+   * Start a reaction collector and switch pages where required.
+   */
+  awaitReactions () {
+    this.reactionCollector = this.menu.createReactionCollector((reaction, user) => user.id === this.userID, { time: 180000 })
+    this.reactionCollector.on('collect', reaction => {
+      if (Object.prototype.hasOwnProperty.call(this.currentPage.reactions, reaction.emoji.name)) {
+        switch (this.currentPage.reactions[reaction.emoji.name]) {
+          case 'first':
+            this.setPage(0)
+            break
+          case 'last':
+            this.setPage(this.pages.length - 1)
+            break
+          case 'previous':
+            if (this.pageIndex > 0) {
+              this.setPage(this.pageIndex - 1)
             }
-        });
-    }
+            break
+          case 'next':
+            if (this.pageIndex < this.pages.length - 1) {
+              this.setPage(this.pageIndex + 1)
+            }
+            break
+          case 'stop':
+            this.stop()
+            break
+          case 'delete':
+            this.delete()
+            break
+          default:
+            // TODO: Sort out documenting this as a TSDoc event.
+            this.setPage(this.pages.findIndex(p => p.name === this.currentPage.reactions[reaction.emoji.name]))
+            break
+        }
+      }
+    })
+  }
 }
